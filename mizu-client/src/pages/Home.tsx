d48ddda38811a62eecf17dcf8b7082c29b713ac9
Avizi Developer <admin@avizi.co>
2026-02-05 15:11:27 -0800

    Add basic skeleton of home page

--- mizu-client/src/pages/Home.tsx
+++ mizu-client/src/pages/Home.tsx
@@ -1,16 +1,53 @@
 import {
+    IonButton,
+    IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,
     IonContent,
     IonPage
 } from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonContent fullscreen>
-      </IonContent>
-    </IonPage>
-  );
+    const decks = ['Deck 1', 'Deck 2', 'Deck 3']
+    return (
+        <IonPage>
+            <IonContent>
+                <div className={'ion-display-flex ion-flex-column ion-justify-content-center ion-align-items-center'}>
+                    <IonCard>
+                        <IonCardContent>
+                            <div className={'ion-display-flex ion-flex-row'}>
+                                <IonButton className={'ion-margin-end'} color={'primary'}>Create Deck</IonButton>
+                                <IonButton color={'tertiary'}>Card Browser</IonButton>
+                            </div>
+                        </IonCardContent>
+                    </IonCard>
+
+                    <IonCard className={'ion-padding'} style={{minWidth: '50%'}}>
+                        <IonCardHeader>
+                            <IonCardTitle>
+                                <div className={'ion-display-flex ion-flex-row ion-justify-content-center'}>
+                                    <span>Decks</span>
+                                </div>
+                            </IonCardTitle>
+                        </IonCardHeader>
+                        <IonCardContent>
+                            {decks.map(deck => (
+                                <div
+                                    className={'ion-margin-bottom ion-display-flex ion-flex-row ion-align-items-center'}>
+                                    <IonButton className={'ion-margin'}
+                                               color={'white'}
+                                               style={{
+                                                   backgroundColor: 'white',
+                                                   fontSize: 18,
+                                                   color: '#16161d',
+                                               }}>{deck}</IonButton>
+                                </div>
+                            ))}
+                        </IonCardContent>
+                    </IonCard>
+                </div>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Home;
