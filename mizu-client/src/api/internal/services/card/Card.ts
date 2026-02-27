6e76732e45fef3038a0f4017c79ee66a26e71a0f
avizi-developer <admin@avizi.co>
2026-02-27 11:47:31 -0800

    Wip

--- mizu-client/src/api/internal/services/card/Card.ts
+++ mizu-client/src/api/internal/services/card/Card.ts
@@ -1,6 +1,5 @@
-import {CardTemplate} from "./CardTemplate";
+import {basicCardTemplate, CardTemplate} from "./CardTemplate";
 import {CardType, CardTypeToSchemaMap} from "./CardType";
-import * as handlebars from "handlebars";
 
 export type AnyCard = CardType extends any ? Card<CardType> : never;
 
@@ -36,4 +35,54 @@ export function renderCardFrontToHtml(card: AnyCard): string {
 
 export function renderCardBackToHtml(card: AnyCard): string {
     return renderCardSideToHtml(card, CardSide.BACK);
+}
+
+/**
+ *
+ * @param cardType
+ * @param fields
+ * @param customCardTemplate - If the given `cardType` is `CardType.Custom`, then `customCardTemplate` is required.
+ */
+export async function createCard<T extends CardType>(cardType: T, fields: CardTypeToSchemaMap[T], customCardTemplate?: CardTemplate): Promise<Card<T>> {
+    // todo custom card template param - probably better to just use template id lookup
+    if (customCardTemplate && cardType !== CardType.Custom) {
+        throw new Error('[CreateCard] Card type is not custom, but custom card template was given. ')
+    }
+
+    if (cardType === CardType.Basic) {
+        return {
+            id: 'random', // todo
+            fields,
+            template: basicCardTemplate,
+            due: new Date() // todo
+        } as Card<T>;
+    } else throw new Error('[CreateCard] Invalid card type given.')
+}
+
+export async function createBasicCard(fields: BasicCardSchema) {
+    return createCard(CardType.BASIC, fields);
+}
+
+export async function createBasicAndReversedCard(front: HtmlRawString, back: HtmlRawString) {
+    return createCard(CardType.BasicAndReversed, {front, back});
+}
+
+export async function createClozeCard(front: HtmlRawString, text: HtmlRawString, extraText: HtmlRawString) {
+    return createCard<CardType.Cloze>({front, back: {text, extraText}});
+}
+
+export async function createImageOcclusionCard(front: HtmlRawString, back: HtmlRawString) {
+    return createCard<CardType.ImageOcclusion>({front, back})
+}
+
+export async function createCardFromCustomType(values: CustomCardSchema) {
+    return createCard<CardType.Custom>(values);
+}
+
+export async function modifyCard(cardId: string) {
+    // todo
+}
+
+export async function deleteCard(cardId: string) {
+    // todo - make sure to delete associated media
 }
--- mizu-client/src/api/internal/services/card/CardTemplate.ts
+++ mizu-client/src/api/internal/services/card/CardTemplate.ts
@@ -1,29 +1,73 @@
 import * as handlebars from "handlebars";
+import {CardType} from "./CardType";
+
+export type TemplateId = string;
 
 /**
  * The template strings are html/css/js that are compatible with handlebars,
  * using double braces, for example {{var1}}, around variables.
  */
 export interface CardTemplate {
+    id: TemplateId,
     frontHtmlTemplateCompiled: HandlebarsTemplateDelegate;
     backHtmlTemplateCompiled: HandlebarsTemplateDelegate;
 }
 
-export const basicCardFrontHtmlTemplateString: string = `
-<div>{{front}}</div>
-`;
+// todo will be loaded from folder
+const allCardTemplates: Map<TemplateId, CardTemplate> = new Map();
 
-export const basicCardFrontTemplateCompiled = handlebars.compile(basicCardFrontHtmlTemplateString);
+export function createCardTemplate(templateId: TemplateId, frontHtmlTemplateString: string, backHtmlTemplateString: string) {
+    if (allCardTemplates.has(templateId)) {
+        throw new Error('[CreateCardTemplate] Card template with given id already exists.')
+    }
 
-export const basicCardBackHtmlTemplateString: string = `
-<div>{{front}}</div>
-<hr/>
-<div>{{back}}</div>
-`;
+    const cardTemplate: CardTemplate = {
+        id: templateId,
+        frontHtmlTemplateCompiled: handlebars.compile(frontHtmlTemplateString),
+        backHtmlTemplateCompiled: handlebars.compile(backHtmlTemplateString)
+    };
 
-export const basicCardBackTemplateCompiled = handlebars.compile(basicCardBackHtmlTemplateString);
-
-export const basicCardTemplate: CardTemplate = {
-    frontHtmlTemplateCompiled: basicCardFrontTemplateCompiled,
-    backHtmlTemplateCompiled: basicCardBackTemplateCompiled,
+    allCardTemplates.set(templateId, cardTemplate);
+    return cardTemplate;
 }
+
+export function getAllCardTemplates() {
+    return Array.from(allCardTemplates.values());
+}
+
+export function getCardTemplate(templateId: TemplateId) {
+    return allCardTemplates.get(templateId);
+}
+
+export function modifyCardTemplate(templateId: TemplateId, frontHtmlTemplateString: string, backHtmlTemplateString: string) {
+    if (!allCardTemplates.has(templateId)) {
+        throw new Error('[ModifyCardTemplate] Card template with given id does not exist.')
+    }
+
+    deleteCardTemplate(templateId);
+    return createCardTemplate(templateId, frontHtmlTemplateString, backHtmlTemplateString)
+}
+
+export function deleteCardTemplate(templateId: TemplateId) {
+    return allCardTemplates.delete(templateId);
+}
+
+export function createBasicCardTemplate() {
+    const basicCardFrontHtmlTemplateString: string = `
+        <div>{{front}}</div>
+    `;
+
+    const basicCardBackHtmlTemplateString: string = `
+        <div>{{front}}</div>
+        <hr/>
+        <div>{{back}}</div>
+    `;
+
+    return createCardTemplate(CardType.Basic, basicCardFrontHtmlTemplateString, basicCardBackHtmlTemplateString);
+}
+
+
+export const basicCardTemplate: CardTemplate = createBasicCardTemplate();
+
+// todo - rest of card types
+
